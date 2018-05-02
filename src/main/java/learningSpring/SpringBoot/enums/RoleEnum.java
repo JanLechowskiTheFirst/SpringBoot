e3a3831db3deb004552eaf17d3c4aada4517c339
Jan <youneedtominemoreminerals@gmail.com>
2018-05-02 20:04:43 +0200

    Small refactor of 2 services + the rest of unit tests.

---
+++ src/main/java/learningSpring/SpringBoot/enums/RoleEnum.java
@@ -0,0 +1,15 @@
+package learningSpring.SpringBoot.enums;
+
+public enum RoleEnum {
+    ADMIN {
+        public String toString() {
+            return "ROLE_admin";
+        }
+    },
+
+    USER {
+        public String toString() {
+            return "ROLE_user";
+        }
+    }
+}
