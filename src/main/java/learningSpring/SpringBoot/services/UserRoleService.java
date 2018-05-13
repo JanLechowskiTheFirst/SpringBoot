2402f7e364d0ee998fbfcf3071067682bbcbfe69
Jan <youneedtominemoreminerals@gmail.com>
2018-05-13 17:22:40 +0200

    Logging non standard situations in userroleservice + change in loggin

--- src/main/java/learningSpring/SpringBoot/services/UserRoleService.java
+++ src/main/java/learningSpring/SpringBoot/services/UserRoleService.java
@@ -5,7 +5,11 @@ import learningSpring.SpringBoot.enums.RoleEnum;
 import learningSpring.SpringBoot.repositories.RoleRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.management.RuntimeErrorException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class
@@ -14,8 +18,9 @@ UserRoleService {
     @Autowired
     RoleRepo roleRepo;
 
-    public boolean hasRoleAdmin(List<UserRole> role, int userId){
+    private static final Logger LOGGER = Logger.getLogger(UserRoleService.class.getName() );
 
+    public boolean hasRoleAdmin(List<UserRole> role, int userId){
         for(UserRole r:role){
             if(r.getUserId() == userId && r.getUserRole() == RoleEnum.ADMIN)
                 return true;
@@ -34,6 +39,10 @@ UserRoleService {
         if(!hasRoleAdmin(role, userId)) {
             roleRepo.saveAndFlush(adminRole);
         }
+        else{
+            LOGGER.log( Level.FINE, "ERROR: Adding ADMIN rights for ADMIN user");
+
+        }
     }
 
     public void deleteAdminRole(int userId) {
@@ -41,6 +50,10 @@ UserRoleService {
         UserRole adminRole = new UserRole(role.get(0).id, userId, RoleEnum.ADMIN);
         if(hasRoleAdmin(role, userId)) {
             roleRepo.delete(adminRole);
+        }
+        else{
+            LOGGER.log( Level.FINE, "ERROR: Deleting ADMIN rights from non ADMIN user");
+
         }
     }
 }
